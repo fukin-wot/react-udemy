cde30bdb4677208328a209a8bd28544c4c72ef4e
sacros <33897080+fukin-wot@users.noreply.github.com>
2017-12-13 09:58:20 +0530

    module 6

--- 6. Debugging react apps/react-complete-guide/src/App.js
+++ 6. Debugging react apps/react-complete-guide/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -50,12 +51,12 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
+            return <ErrorBoundary key={person.id}><Person
               name={person.name}
               age={person.age}
+              id={person.id}
               click={() => this.deletePersonHandler(index)}
-              key={person.id}
-              changed={(event) => this.nameChangedHandler(event, person.id)} />
+              changed={(event) => this.nameChangedHandler(event, person.id)} /></ErrorBoundary>
           })}
         </div>
       );
